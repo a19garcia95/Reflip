b41e7dd3bfb587758e9366c1bd1578fe462abcbe
juan.arredondo <juan.arredondo@gmail.com>
2016-11-28 18:18:06 -0500

    displaying result from api

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,23 +1,24 @@
 class Property < ApplicationRecord
-  def self.create_from_temp_property(temp_prop, user_id)
-    create(estimate: temp_prop.estimate, user_id: user_id)
-  end
+#   def self.create_from_temp_property(temp_prop, user_id)
+#     create(estimate: temp_prop.estimate, user_id: user_id)
+#   end
+#
+#
+# # api_results params[:address], params[:citystatezip]
+#   def api_results(address, citystatezip)
+#     # @address = params[:address].to_s.gsub(" ","+")
+#      @address = address.to_s.gsub(" ","+")
+#
+#     # @citystatezip = params[:citystatezip].to_s.gsub(" ", "+").gsub(/,/,"%2C")
+#     @citystatezip = citystatezip.to_s.gsub(" ", "+").gsub(/,/,"%2C")
+#     if (@address and @citystatezip)
+#       @zillow_api = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19hai5nqayz_6ttia&address=#{@address}&citystatezip=#{@citystatezip}"
+#       @results = HTTParty.get(@zillow_api)
+#
+#   end
+#
+#   def estimate(results)
+#     estimate = results["searchresults"]["response"]["results"]["result"]["zestimate"]["amount"]["__content__"]
+#   end
 
-
-# api_results params[:address], params[:citystatezip]
-  def api_results(address, citystatezip)
-    # @address = params[:address].to_s.gsub(" ","+")
-     @address = address.to_s.gsub(" ","+")
-
-    # @citystatezip = params[:citystatezip].to_s.gsub(" ", "+").gsub(/,/,"%2C")
-    @citystatezip = citystatezip.to_s.gsub(" ", "+").gsub(/,/,"%2C")
-    if (@address and @citystatezip)
-      @zillow_api = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19hai5nqayz_6ttia&address=#{@address}&citystatezip=#{@citystatezip}"
-      @results = HTTParty.get(@zillow_api)
-
-  end
-
-  def estimate(results)
-    estimate = results["searchresults"]["response"]["results"]["result"]["zestimate"]["amount"]["__content__"]
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'properties#index'
+
+  get '/properties', to: 'properties#show'
 end
